39599af940b9fd96e7ab8782119091519570e73a
giathai1505 <thai.ngo@usumsoftware.com>
2023-09-04 19:49:17 +0700

    feat: UI design page

---
+++ src/pages/Design/ChooseDesign.tsx
@@ -0,0 +1,238 @@
+import Dropdown from "@/components/Common/Dropdown";
+import React, { useState } from "react";
+
+const listMaterials = [
+  {
+    id: 1,
+    name: "Da Lemals",
+    popularPercent: 30,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+  },
+  {
+    id: 2,
+    name: "Da Estoril",
+    popularPercent: 60,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+  },
+  {
+    id: 3,
+    name: "Da Napalux",
+    popularPercent: 90,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+  },
+  {
+    id: 4,
+    name: "Da Milan",
+    popularPercent: 100,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+  },
+];
+
+const listColors = [
+  {
+    id: 1,
+    color: "#F2E3C6",
+  },
+  {
+    id: 2,
+    color: "#BA804C",
+  },
+  {
+    id: 3,
+    color: "#C44B3B",
+  },
+];
+
+const listHolds = [
+  {
+    id: 1,
+    color: "#F2E3C6",
+  },
+  {
+    id: 2,
+    color: "#BA804C",
+  },
+  {
+    id: 3,
+    color: "#C44B3B",
+  },
+];
+
+interface IDesign {
+  materialID: number | undefined;
+  colorID: number | undefined;
+  holeID: number | undefined;
+  note: string;
+}
+
+interface IChooseDesign {
+  onNext: () => void;
+  onPrevious: () => void;
+}
+
+const ChooseDesign: React.FC<IChooseDesign> = ({ onNext, onPrevious }) => {
+  const [selectedMaterial, setSelectedMaterial] = useState<IDesign>({
+    materialID: undefined,
+    colorID: undefined,
+    holeID: undefined,
+    note: "",
+  });
+
+  const handleChangeDesign = (value: any) => {
+    let newState = { ...selectedMaterial, ...value };
+    setSelectedMaterial(newState);
+  };
+
+  console.log("=============", selectedMaterial);
+
+  return (
+    <div className="max-w-[1200px] mx-auto">
+      <div>
+        <h3 className="design-title">Chọn chất liệu da</h3>
+        <br />
+        <div className="grid grid-cols-4 gap-10">
+          {listMaterials.map((item) => {
+            return (
+              <div
+                className={`border-[1px] border-solid text-white p-6 relative flex flex-col gap-4 cursor-pointer ${
+                  selectedMaterial.materialID !== item.id
+                    ? "border-gray-400"
+                    : "border-primary"
+                }`}
+                onClick={() => handleChangeDesign({ materialID: item.id })}
+              >
+                <div className="flex items-center gap-2">
+                  <label htmlFor="" className="">
+                    <input
+                      type="checkbox"
+                      name=""
+                      checked={selectedMaterial.materialID === item.id}
+                      id=""
+                      className="w-6 h-6"
+                    />
+                  </label>
+                  <h4 className="font-bold">{item.name}</h4>
+                </div>
+
+                <p>
+                  <span className="font-bold">Độ phổ biến:</span>{" "}
+                  {item.popularPercent}%
+                </p>
+                <p className="text-justify">
+                  {" "}
+                  <span className="font-bold"> Đặc điểm:</span> <br />
+                  {item.description}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        <h3 className="design-title">Chọn màu da</h3>
+        <br />
+        <div className="grid grid-cols-3 gap-10">
+          {listColors.map((item) => {
+            return (
+              <div
+                className={`border-[1px] border-solid  text-white relative flex flex-col  gap-4 cursor-pointer ${
+                  selectedMaterial.colorID !== item.id
+                    ? "border-0"
+                    : "border-primary"
+                }`}
+                onClick={() => handleChangeDesign({ colorID: item.id })}
+              >
+                <label htmlFor="" className="absolute top-5 left-5">
+                  <input
+                    type="checkbox"
+                    name=""
+                    checked={selectedMaterial.colorID === item.id}
+                    id=""
+                    className="w-6 h-6"
+                  />
+                </label>
+                <div>
+                  <div
+                    className={`w-full h-[200px] bg-[${item.color}] `}
+                    style={{ background: `${item.color}` }}
+                  ></div>
+                  <p className="text-white text-center py-2">
+                    <span>Mã màu: </span>
+                    {item.id}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div>
+        <h3 className="design-title">CHỌN THIẾT KẾ ĐỤC LỖ</h3>
+        <br />
+        <div className="grid grid-cols-3 gap-10">
+          {listHolds.map((item) => {
+            return (
+              <div
+                className={`border-[1px] border-solid  text-white relative flex flex-col  gap-4 cursor-pointer ${
+                  selectedMaterial.holeID !== item.id
+                    ? "border-0"
+                    : "border-primary"
+                }`}
+                onClick={() => handleChangeDesign({ holeID: item.id })}
+              >
+                <label htmlFor="" className="absolute top-5 left-5">
+                  <input
+                    type="checkbox"
+                    name=""
+                    checked={selectedMaterial.holeID === item.id}
+                    id=""
+                    className="w-6 h-6"
+                  />
+                </label>
+                <div>
+                  <div
+                    className={`w-full h-[200px] bg-[${item.color}] `}
+                    style={{ background: `${item.color}` }}
+                  ></div>
+                  <p className="text-white text-center py-2">
+                    <span>Mã màu: </span>
+                    {item.id}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div>
+        <h3 className="design-title">Lưu ý đặc biệt</h3>
+        <br />
+        <textarea
+          name=""
+          id=""
+          cols={30}
+          rows={10}
+          placeholder="Lưu ý của bạn ......"
+          onChange={(e) => handleChangeDesign({ note: e.target.value })}
+        />
+      </div>
+
+      <div className="flex justify-end gap-5 mt-10">
+        <button className="primary-button" onClick={() => onPrevious()}>
+          Trở lại
+        </button>
+        <button className="primary-button" onClick={() => onNext()}>
+          Tiếp theo
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ChooseDesign;
--- src/pages/Design/index.tsx
+++ src/pages/Design/index.tsx
@@ -1,17 +1,144 @@
+"use client";
 import ProgressBar from "@/components/ProgressBar";
-import React from "react";
+import React, { useMemo, useState } from "react";
+import ChooseCar from "./ChooseCar";
+import ChooseDesign from "./ChooseDesign";
+import Conclusion from "./Conclusion";
+import Link from "next/link";
+import car from "@/assets/images/home-slider/home-slider2.jpeg";
+import Image from "next/image";
+import startButton from "@/assets/images/start-now-button.png";
+import Button from "@/components/Common/Button";
 
 const Design = () => {
+  const [phase, setPhase] = useState<number>(1);
+
+  const renderPhase = useMemo(() => {
+    let phaseComponent = <ChooseCar onNext={() => setPhase(2)} />;
+    switch (phase) {
+      case 1:
+        phaseComponent = <ChooseCar onNext={() => setPhase(2)} />;
+
+        break;
+      case 2:
+        phaseComponent = (
+          <ChooseDesign
+            onNext={() => setPhase(3)}
+            onPrevious={() => setPhase(1)}
+          />
+        );
+
+        break;
+      case 3:
+        phaseComponent = <Conclusion />;
+
+        break;
+
+      default:
+        phaseComponent = <ChooseCar onNext={() => setPhase(2)} />;
+        break;
+    }
+    return phaseComponent;
+  }, [phase]);
   return (
-    <div className="design-wrapper ">
-      <div className=" bg-black pb-40">
-        <h2 className="pt-[200px]">
-          <span>CHỌN DÒNG XE BẠN ĐANG SỞ HỮU</span>
-        </h2>
+    <div className="design-wrapper">
+      <div className="bg-black">
+        <div className="mb-20">
+          <h2 className="pt-[200px] text-[27px]">
+            <span>CHỌN DÒNG XE BẠN ĐANG SỞ HỮU</span>
+          </h2>
+        </div>
+        <div className="wrapper">
+          <div className="flex justify-center mb-20">
+            <ProgressBar active={phase} />
+          </div>
+          <div className="pb-40">{renderPhase}</div>
+        </div>
       </div>
-      <div className="wrapper py-10">
-        <div className="flex justify-center">
-          <ProgressBar active={3} />
+      <div className="home-leather ">
+        <div className="container mx-auto max-w-[1200px]">
+          <h2 className="mb-10">
+            Với <span>Lavor</span> , nội thất xe của bạn sẽ trở nên{" "}
+            <span> đẳng cấp</span>
+          </h2>
+          <div className="grid grid-cols-2 gap-20">
+            <div>
+              <Image alt="oto" src={car} className="w-full rounded-md" />
+            </div>
+            <div className="leather-text">
+              <p>
+                Hãy tưởng tượng nội thất xe của bạn được bọc da sang trọng, nâng
+                cấp hiện đại và mang phong cách của riêng bạn. Có thể bạn muốn
+                điều đó nhưng lại lo lắng về chất lượng ở các Garage. Hoặc bạn
+                đang cảm thấy khó khăn khi muốn chiếc ghế da mang phong cách cá
+                nhân của mình trở nên thông minh và hữu ích hơn nữa. Dù mong
+                muốn của bạn là gì, Lavor cũng có thể thực hiện nó, tăng thêm sự
+                sang trọng, tiện nghi và phong cách cho chiếc xe của bạn.
+              </p>
+
+              <p>
+                Lavor là một thương hiệu sản xuất nội thất ô tô, trực thuộc Công
+                ty TNHH TMDV & SX Minh Tâm – chuyên gia trong lĩnh vực sản xuất
+                nội thất, độ xe và dịch vụ lắp đặt nội thất ô tô tại Việt Nam.
+                Qua hơn 10 năm xây dựng và phát triển, trên 1.000 chủng loại sản
+                phẩm được sản xuất với sản lượng 300.000 sản phẩm/năm, Lavor
+                đang dần khẳng định vị thế của mình trên thị trường. "
+              </p>
+            </div>
+          </div>
+          <div className="mt-10">
+            <Button link="" text="Tìm hiểu thêm" />
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-[#2D2D2D] pb-40">
+        <div className="max-w-[1200px] mx-auto text-center py-10">
+          <h2 className="mb-10">
+            CHỌN CHIẾC XE CỦA BẠN - CÒN LẠI <span>LAVOR </span> LO
+          </h2>
+          <div className="grid grid-cols-3 gap-20 mb-10">
+            <div className="flex gap-10 text-left">
+              <p className="p3-big-text">1.</p>
+              <div>
+                <h3>CHỌN THIẾT KẾ CỦA RIÊNG BẠN</h3>
+                <p className="text-white mt-4 font-light">
+                  Bạn chỉ cần tìm chiếc xe của mình bằng cách chọn năm sản xuất,
+                  hãng xe, dòng xe sau đó nhấn tiếp tục để chọn thiết kế riêng
+                  của bạn.
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-10 text-left font-light">
+              <p className="p3-big-text">2.</p>
+              <div>
+                <h3>LAVOR LÊN LỊCH NÂNG CẤP NỘI THẤT XE GIÚP BẠN</h3>
+                <p className="text-white mt-4">
+                  Hãy để lại thông tin liên hệ. Những chuyên viên của Lavor sẽ
+                  tư vấn cụ thể hơn và lên lịch nâng cấp nội thất xe giúp bạn.
+                </p>
+              </div>
+            </div>{" "}
+            <div className="flex gap-10 text-left font-light">
+              <p className="p3-big-text">3.</p>
+              <div>
+                <h3>LAVOR NÂNG CẤP NỘI THẤT XE GIÚP BẠN</h3>
+                <p className="text-white mt-4">
+                  Lavor sẽ tiến hành nâng cấp nội thất xe và mang đến bất ngờ
+                  cho bạn.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-20 flex justify-center">
+            <Link href="#">
+              <Image
+                src={startButton}
+                alt="Chọn thiêt kế riêng cuả bạn"
+                className="w-80 start-button"
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </div>

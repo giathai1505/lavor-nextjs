117275d596ad656d523d79736f434a0d8df103ff
Gia Thai <thai.ngo@s3corp.com.vn>
2023-12-26 15:56:03 +0700

    fix: update the sideBar header phone using Drawer of antd

---
+++ src/components/Header/PhoneDrawerNavbar.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React from "react";
+import { userNavbarData } from "@/assets/staticData";
+import { Drawer } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import logo from "@/assets/images/logo/logo-white.png";
+import { MdClose } from "react-icons/md";
+
+type TProps = {
+  show: boolean;
+  setShow: (value: boolean) => void;
+};
+
+const PhoneDrawerNavbar: React.FC<TProps> = ({ show, setShow }) => {
+  const path = usePathname();
+  return (
+    <Drawer
+      title={
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <Image src={logo} alt="Logo" className={`w-[100px] logo-img`} />
+          </Link>
+        </div>
+      }
+      closeIcon={<MdClose className="text-white"/>}
+      placement="right"
+      onClose={() => setShow(false)}
+      open={show}
+      style={{ background: "black" }}
+    >
+      <div className="text-white py-5">
+        {userNavbarData.map((item) => {
+          return (
+            <div className="nav-item-phone" key={item.id}>
+              <Link
+                href={item.slug}
+                className={`cursor-pointer navbar-item px-5 py-5 block ${
+                  path?.toString() === item.slug ? "text-primary" : "text-white"
+                }`}
+              >
+                {item.name}
+              </Link>
+              <div className="sub-nav -left-1/2 absolute translate-x-1/3">
+                <ul className="mt-5">
+                  {item.children && item.children.length > 0
+                    ? item.children.map((child, index) => {
+                        return (
+                          <li
+                            className="relative mb-2 bg-primary text-center nav-sub-item leading-3 w-32"
+                            key={index.toString()}
+                          >
+                            <Link
+                              href={child?.slug}
+                              className="relative z-10 py-2 block"
+                            >
+                              <span className="z-10 text-white uppercase text-[130x] hover:text-[#2d2d2d]">
+                                {child.name}
+                              </span>
+                            </Link>
+                          </li>
+                        );
+                      })
+                    : null}
+                </ul>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </Drawer>
+  );
+};
+
+export default PhoneDrawerNavbar;
--- src/components/Header/PhoneHeader.tsx
+++ src/components/Header/PhoneHeader.tsx
@@ -4,17 +4,14 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import logo from "@/assets/images/logo/logo-white.png";
 import { BsList } from "react-icons/bs";
-import { MdClose } from "react-icons/md";
-import { usePathname } from "next/navigation";
-import { userNavbarData } from "@/assets/staticData";
 import { IProduct } from "@/types/type";
 import { ee } from "@/pages/DetailProduct/DetailContent";
 import Cart from "./Cart";
 import ConfirmOrderModel from "./ConfirmOrderModel";
+import PhoneDrawerNavbar from "./PhoneDrawerNavbar";
 
 const PhoneHeader = () => {
   const [showSidebar, setShowSidebar] = useState<boolean>(false);
-  const path = usePathname();
   const [carts, setCarts] = useState<IProduct[]>([]);
   const [confirmModelOpen, setConfirmModelOpen] = useState<boolean>(false);
 
@@ -22,6 +19,7 @@ const PhoneHeader = () => {
     setCarts(carts);
   };
 
+  //listen add to cart event then update the carts
   ee.on("addToCart", handleEventAddToCart);
 
   useEffect(() => {
@@ -57,68 +55,8 @@ const PhoneHeader = () => {
         </div>
       </div>
 
-      <div
-        className={`fixed inset-0 z-40 bg-[#0000006e] ${
-          showSidebar ? "" : "hidden"
-        }`}
-      ></div>
-
-      <div
-        className={`header-phone-menubar w-[400px] ${
-          showSidebar ? "left-0" : "left-[100%]"
-        }`}
-      >
-        <div className="flex items-center justify-between">
-          <Link href="/">
-            <Image src={logo} alt="Logo" className={`w-[100px] logo-img`} />
-          </Link>
-          <MdClose
-            className="w-[30px] h-[30px] text-white cursor-pointer"
-            onClick={() => setShowSidebar(false)}
-          />
-        </div>
-        <div className="text-white py-5">
-          {userNavbarData.map((item) => {
-            return (
-              <div className="nav-item-phone" key={item.id}>
-                <Link
-                  href={item.slug}
-                  className={`cursor-pointer navbar-item px-5 py-5 block ${
-                    path?.toString() === item.slug
-                      ? "text-primary"
-                      : "text-white"
-                  }`}
-                >
-                  {item.name}
-                </Link>
-                <div className="sub-nav -left-1/2 absolute translate-x-1/3">
-                  <ul className="mt-5">
-                    {item.children && item.children.length > 0
-                      ? item.children.map((child, index) => {
-                          return (
-                            <li
-                              className="relative mb-2 bg-primary text-center nav-sub-item leading-3 w-32"
-                              key={index.toString()}
-                            >
-                              <Link
-                                href={child?.slug}
-                                className="relative z-10 py-2 block"
-                              >
-                                <span className="z-10 text-white uppercase text-[130x] hover:text-[#2d2d2d]">
-                                  {child.name}
-                                </span>
-                              </Link>
-                            </li>
-                          );
-                        })
-                      : null}
-                  </ul>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <PhoneDrawerNavbar setShow={setShowSidebar} show={showSidebar} />
+      
       <ConfirmOrderModel
         carts={carts}
         setCarts={setCarts}
